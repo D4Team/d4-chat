9d04220baeed43c46b421d95d73df01264fb8c1c
Aleksei Litkovetc <turgorss@gmail.com>
2023-04-06 17:55:21 +0300

    D4CHAT-25 feat: Add join functionality for chat rooms

---
+++ src/main/scala/ru/d4team/chat/services/ChatRoomsService.scala
@@ -0,0 +1,36 @@
+package ru.d4team.chat.services
+
+import cats.implicits._
+import zio._
+import zio.http.Channel
+import zio.http.socket.WebSocketFrame
+import ru.d4team.chat.services.ChatRoomsService._
+
+import scala.collection.concurrent.TrieMap
+
+trait ChatRoomsService[Storage] {
+  private[services] def rooms: Storage
+
+  def join(channel: Channel[WebSocketFrame], room: String): UIO[Unit]
+}
+
+object ChatRoomsService {
+  type ChatRoomMap  = TrieMap[String, Channel[WebSocketFrame]]
+  type ChatRoomsMap = TrieMap[String, ChatRoomMap]
+
+  val mapStorageLive: ULayer[ChatRoomsService[ChatRoomsMap]] = ZLayer.fromZIO {
+    ZIO.succeed(TrieMap.empty[String, ChatRoomMap]).map(ChatRoomsServiceMapImpl)
+  }
+}
+
+final case class ChatRoomsServiceMapImpl(private[services] val rooms: ChatRoomsMap)
+    extends ChatRoomsService[ChatRoomsMap] {
+
+  def join(channel: Channel[WebSocketFrame], room: String): UIO[Unit] =
+    ZIO.succeed {
+      rooms.updateWith(room) {
+        case Some(room) => room.addOne(channel.id, channel).some
+        case None       => TrieMap(channel.id -> channel).some
+      }
+    }.unit
+}
---
+++ src/test/scala/ru/d4team/chat/utils/TestWebSocketChannel.scala
@@ -0,0 +1,45 @@
+package ru.d4team.chat.utils
+
+import zio._
+import zio.http._
+import zio.http.socket._
+import zio.http.ChannelEvent._
+
+import java.util.UUID
+
+object TestWebSocketChannel {
+  def make: UIO[WebSocketChannel] =
+    for {
+      queue <- Queue.unbounded[ChannelEvent.Event[WebSocketFrame]]
+      _     <- queue.offer(UserEventTriggered(UserEvent.HandshakeComplete))
+    } yield TestWebSocketChannel(queue)
+}
+
+final case class TestWebSocketChannel(counterpartEvents: Queue[ChannelEvent.Event[WebSocketFrame]])
+    extends WebSocketChannel {
+
+  private val baseId = UUID.randomUUID.toString
+
+  override def id(implicit trace: Trace): String = baseId
+
+  override def awaitClose(implicit trace: Trace): UIO[Unit] = close(await = true).orDie
+
+  override def close(await: Boolean)(implicit trace: Trace): Task[Unit] =
+    counterpartEvents.offer(ChannelEvent.ChannelUnregistered).unit
+
+  override def flush(implicit trace: Trace): Task[Unit] = ZIO.unit
+
+  override def write(msg: WebSocketFrame, await: Boolean)(implicit trace: Trace): Task[Unit] =
+    counterpartEvents.offer(ChannelEvent.ChannelRead(msg)).unit
+
+  override def writeAndFlush(msg: WebSocketFrame, await: Boolean)(implicit trace: Trace): Task[Unit] =
+    counterpartEvents.offer(ChannelEvent.ChannelRead(msg)).unit
+
+  override def autoRead(flag: Boolean)(implicit trace: Trace): UIO[Unit] = ???
+
+  override def contramap[A1](f: A1 => WebSocketFrame): Channel[A1] = ???
+
+  override def isAutoRead(implicit trace: Trace): UIO[Boolean] = ???
+
+  override def read(implicit trace: Trace): UIO[Unit] = ???
+}
